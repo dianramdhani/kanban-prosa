33e79f9d5ef7be88fc34a9aa0075ffebeed6d5ce
dianramdhani <dian.ramdhani@gmail.com>
2021-09-07 11:05:57 +0700

    init vuedraggable lib

---
+++ src/components/Todo.vue
@@ -0,0 +1,37 @@
+<template>
+  <draggable
+    v-model="myArray"
+    group="people"
+    @start="drag = true"
+    @end="drag = false"
+    item-key="id"
+  >
+    <template #item="{ element }">
+      <div>{{ element.name }}</div>
+    </template>
+  </draggable>
+</template>
+
+<script>
+import draggable from "vuedraggable";
+
+export default {
+  name: "Todo",
+  components: {
+    draggable,
+  },
+  data() {
+    return {
+      drag: false,
+      myArray: [
+        {
+          name: "dian",
+        },
+        {
+          name: "ramdhani",
+        },
+      ],
+    };
+  },
+};
+</script>
